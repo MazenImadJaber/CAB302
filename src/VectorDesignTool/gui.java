fc2ee342165775aa0f34f00e362fdf64037d00b9
Mazen Imad Jaber <Mazenimad.jaber@connect.qut.edu.au>
2019-05-13 18:30:21 +1000

    start1

--- src/VectorDesignTool/gui.java
+++ src/VectorDesignTool/gui.java
@@ -1,4 +1,64 @@
 package VectorDesignTool;
 
-public class gui {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class gui extends JFrame implements ActionListener, Runnable{
+    public static final int WIDTH = 300;
+    public static final int HEIGHT = 300;
+    private JPanel pnlDisplay, pnlEast, pnlUp, pnlWest, pnlBtn;
+    private Graphics g;
+
+    public gui(String title) throws HeadlessException {
+        super(title);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+
+    @Override
+    public void run() {
+        createGUI();
+    }
+
+    private void createGUI() {
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        pnlDisplay = createPanel(Color.white);
+        pnlEast = createPanel(Color.lightGray);
+        pnlUp = createPanel(Color.lightGray);
+        pnlWest = createPanel(Color.lightGray);
+        pnlBtn = createPanel(Color.lightGray);
+        getContentPane().add(pnlDisplay,BorderLayout.CENTER);
+        getContentPane().add(pnlEast,BorderLayout.EAST);
+        getContentPane().add(pnlUp,BorderLayout.NORTH);
+        getContentPane().add(pnlWest,BorderLayout.WEST);
+        getContentPane().add(pnlBtn,BorderLayout.SOUTH);
+        g.drawLine(0,0,10,10);
+
+        repaint();
+        setVisible(true);
+    }
+
+    private JPanel createPanel(Color c) {
+        //Create a JPanel object and store it in a local var
+        JPanel pan = new JPanel();
+        //set the background colour to that passed in c
+        pan.setBackground(c);
+        //Return the JPanel object
+        return pan;
+    }
+
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(new gui("BorderLayout"));
+
+
+    }
 }
+
